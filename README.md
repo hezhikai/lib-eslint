0736d7bd7f26fd212ec1410a49363936ce9f58ac
hezhikai <hezhikai@huafeng.com>
2021-05-26 10:42:05 +0800

    eslint 配置

---
+++ README.md
@@ -0,0 +1,13 @@
+### 使用方式
+
+需在发布到 `npm` 后方可使用
+
+在引用的项目中安装依赖 `eslint-config-hzkweb`
+
+在引用的项目中配置 `eslintrc.js` 文件
+
+```javascript
+module.exports = {
+  extends: ['eslint-config-hzkweb']
+};
+```
---
+++ index.js
@@ -0,0 +1,28 @@
+const rules = require('./rules');
+const prettierrc = require('lib-package').prettier;
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    amd: true,
+    es6: true,
+    jest: false
+  },
+  globals: {}, // readonly、writable
+  extends: ['plugin:vue/essential', 'eslint:recommended', 'prettier'],
+  plugins: ['html', 'import', 'json', 'node', 'promise', 'vue', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', prettierrc], // 不符合 prettier 规则的代码，要进行错误提示（红线）
+    ...rules
+  }
+};
